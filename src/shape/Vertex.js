d14b4e683ad2979e52acceaeab572626faf09887
XavCo7 <xcossettini@rogers.com>
2015-12-25 13:08:25 -0500

    Added softbody configuration for each vertex

--- src/shape/Vertex.js
+++ src/shape/Vertex.js
@@ -7,6 +7,11 @@ OIMO.Vertex = function(x, y, z){
 	this.x = x;
 	this.y = y;
 	this.z = z;
+
+	// Soft body config
+	this.restLength = 0;
+	this.stiffness = 0;
+
 	this.uses = []; // Array of faces this vertex is connected to
 	this.elements = [x, y, z];
 };
