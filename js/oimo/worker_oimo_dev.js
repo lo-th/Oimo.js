2fa3447247e151aca7de0be4e037a25edbae39d5
XavCo7 <xcossettini@rogers.com>
2016-01-12 18:30:48 -0500

    Removed stats usage

--- js/oimo/worker_oimo_dev.js
+++ js/oimo/worker_oimo_dev.js
@@ -274,7 +274,7 @@ var userKey = function(key){
 
 var createWorld = function(){
 
-    world = new OIMO.World( dt, broadPhase, iterations );
+    world = new OIMO.World( dt, broadPhase, iterations, false );
 
     timerStep = dt * 1000;
     world.gravity = new OIMO.Vec3(0, Gravity, 0);
