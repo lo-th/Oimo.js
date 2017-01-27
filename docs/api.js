c6e59a517821adf8dc31c7e58f93235cb7f9225f
xprogram <xprogram7@gmail.com>
2017-01-27 10:52:58 -0500

    up

---
+++ docs/api.js
@@ -0,0 +1,16 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "BoxShape",
+        "CylinderShape",
+        "MassInfo",
+        "Proxy",
+        "Shape",
+        "ShapeConfig",
+        "SphereShape"
+    ],
+    "modules": [],
+    "allModules": [],
+    "elements": []
+} };
+});
